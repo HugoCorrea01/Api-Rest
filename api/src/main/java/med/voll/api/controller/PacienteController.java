33db9afd9f3db9079a4fc59a483069f7f813e80a
hugocorrea01 <hcalixto2812@gmail.com>
2025-02-13 10:53:35 -0300

    atualizado com Cadastro de paciente

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,26 +1,36 @@
 package med.voll.api.controller;
 
-import jakarta.transaction.Transactional;
+
 import jakarta.validation.Valid;
 import med.voll.api.Paciente.DadosCadastroPaciente;
+import med.voll.api.Paciente.DadosListagemPaciente;
 import med.voll.api.Paciente.Paciente;
 import med.voll.api.Paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
-
 public class PacienteController {
-    @Autowired
+@Autowired
     private PacienteRepository repository;
 
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
-       repository.save(new Paciente(dados));
+
+        repository.save(new Paciente(dados));
+
+    }
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+
+
     }
 }
